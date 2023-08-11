68584b0c2095167ae5d4f13a57364201a4686d20
Diogo Valente <diogovalente@hotmail.com.br>
2023-08-11 13:59:06 -0300

    Projeto inicial

---
+++ src/main/java/br/com/apitestesjunitmockito/ApiTestesJunitMockitoApplication.java
@@ -0,0 +1,13 @@
+package br.com.apitestesjunitmockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTestesJunitMockitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiTestesJunitMockitoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/apitestesjunitmockito/ApiTestesJunitMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.apitestesjunitmockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTestesJunitMockitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
